18768224c8e6213d1a8696d8ba09112702a5396e
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-15 00:26:56 +0300

    Updates

---
+++ precache-manifest.cacaa175167253cd287c54d5eb02b90e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbd42f8d724b629b4049b7542154709",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "ece05f55b0e4ee687438",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.1f57219b.chunk.css"
+  },
+  {
+    "revision": "032b5cf50d7fb88d25bb",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.a36c90f2.chunk.js"
+  },
+  {
+    "revision": "ece05f55b0e4ee687438",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.4bf49503.chunk.js"
+  },
+  {
+    "revision": "249656bcd6aee9fe97fe",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.16247d91.js"
+  }
+]);
